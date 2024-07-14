a05008415eae8434f85a0259bd83f9bae66bb629
rebecca <rlerer3@gmail.com>
2024-07-14 10:27:17 -0400

    commit

--- Capstone/src/app/header/header.component.ts
+++ Capstone/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ShoppingCartComponent } from '../shopping-cart/shopping-cart.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
