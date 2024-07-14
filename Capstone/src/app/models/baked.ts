bbcc3250282a49a9d7458fb1bc8dcbeaa3d50a11
rebecca <rlerer3@gmail.com>
2024-07-13 22:52:53 -0400

    commit

--- Capstone/src/app/models/baked.ts
+++ Capstone/src/app/models/baked.ts
@@ -3,10 +3,10 @@ import { Bake } from '../models/bake';
 export const BAKED: Bake[] = [
         { id: 1, name: 'double chocolate cupcake', price: 1.20 , unit:"piece", imageURL: "Chocolate Cupcake"},
         { id: 2, name: 'red velvet cupcake', price: 1.20, unit:"piece", imageURL: "Berries Summer Cupcakes"} ,
-        { id: 3, name: 'banana cake', price: 2, unit:"pound"},
-        { id: 4, name: 'cheesecake minis', price: 1.99 ,unit:"piece"},
+        { id: 3, name: 'chocolate summer berries', price: 2, unit:"pound",imageURL: "Berries Summer Cupcakes"},
+        { id: 4, name: 'cheesecake minis', price: 1.99 ,unit:"piece",imageURL: "Berries Summer Cupcakes"},
         { id: 5, name: 'cinnamon bun', price: 1.30 ,unit:"piece", imageURL: "Pumpkin Spice Cake"},
-        { id: 6, name: 'coffee cake', price: 2,unit:"pound"} ,
-        { id: 7, name: 'donut', price: 1,unit:"piece"},
-        { id: 8, name: 'cherry pie', price: 2,unit:"pound"},
+        { id: 6, name: 'coffee cake', price: 2,unit:"pound",imageURL: "Berries Summer Cupcakes"} ,
+        { id: 7, name: 'donut', price: 1,unit:"piece",imageURL: "Berries Summer Cupcakes"},
+        { id: 8, name: 'cherry pie', price: 2,unit:"pound",imageURL: "Berries Summer Cupcakes"},
     ];
