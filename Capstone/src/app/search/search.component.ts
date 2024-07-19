77c6c5dac58caa3fad392ffa460f98f7beedd83d
rebecca <rlerer3@gmail.com>
2024-07-19 13:53:19 -0400

    commit

--- Capstone/src/app/search/search.component.ts
+++ Capstone/src/app/search/search.component.ts
@@ -1,31 +1,3 @@
-// import { Component } from '@angular/core';
-// import { EventEmitter, OnInit, Output } from '@angular/core';
-// import { Bake } from '../models/bake';
-// import { BAKED } from '../models/baked';
-
-// @Component({
-//   selector: 'app-search',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './search.component.html',
-//   styleUrl: './search.component.css'
-// })
-// export class SearchComponent {
-//   bake:Bake[] = [];
-//   search() {
-//     if (this.searchText === '' || !this.searchText)
-//     this.bake = BAKED;
-//   else {
-//     this.bake = BAKED;
-//     this.bake = this.bake.filter(bake => bake.name?.startsWith(this.searchText.toLowerCase()));
-//   }
-
-//   }
-//   reset() {
-//     this.searchText = '';
-//     this.bake = BAKED;
-//   }
-// }
 
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
