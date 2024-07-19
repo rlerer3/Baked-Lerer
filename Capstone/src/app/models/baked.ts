cc6f4a2c7f41dfcb3c14a2c4cc6a786c7ae64b36
rebecca <rlerer3@gmail.com>
2024-07-19 13:49:50 -0400

    commit

--- Capstone/src/app/models/baked.ts
+++ Capstone/src/app/models/baked.ts
@@ -1,11 +1,11 @@
 import { Bake } from '../models/bake';
-//copy and make pics
+
 export const BAKED: Bake[] = [
         { id: 1, name: 'double chocolate cupcake', price: 1.20 , unit:"piece", imageURL: "Chocolate Cupcake"},
         { id: 2, name: 'red velvet cupcake', price: 1.20, unit:"piece", imageURL: "Berries Summer Cupcakes"} ,
         { id: 3, name: 'chocolate summer berries', price: 2, unit:"pound",imageURL: "Berries Summer Cupcakes"},
         { id: 4, name: 'cheesecake minis', price: 1.99 ,unit:"piece",imageURL: "Berries Summer Cupcakes"},
-        { id: 5, name: 'cinnamon bun', price: 1.30 ,unit:"piece", imageURL: "Pumpkin Spice Cake"},
+        { id: 5, name: 'pumpkin spice cake', price: 1.30 ,unit:"piece", imageURL: "Pumpkin Spice Cake"},
         { id: 6, name: 'coffee cake', price: 2,unit:"pound",imageURL: "Berries Summer Cupcakes"} ,
         { id: 7, name: 'donut', price: 1,unit:"piece",imageURL: "Berries Summer Cupcakes"},
         { id: 8, name: 'cherry pie', price: 2,unit:"pound",imageURL: "Berries Summer Cupcakes"},
